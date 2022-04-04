50ddb1cc948d0036fc18a697b5ae14f481f64f23
Suna Erkan <ranalekesizqgmail.com>
2022-04-04 22:20:03 +0300

    04.04.2022

--- src/src/practice_basic_day06/Q09_ZoneDateTime.java
+++
@@ -1,9 +0,0 @@
-package src.practice_basic_day06;
-
-public class Q09_ZoneDateTime {
-    // Baska ülke veya bolgelerin saat dilimine gore zamani alma
-    // Newyork un saat dilimine gore saat ?
-    public static void main(String[] args) {
-
-    }
-}
